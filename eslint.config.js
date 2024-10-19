92b39b94da28a1f7def466732309cc128b2b7e8f
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-10-19 16:35:53 -0500

    ✨ eslint

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,14 @@ module.exports = [
     // Override or add rules here
     rules: {
       'semi': 'error',
-       "no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 1 }]
+      'no-multiple-empty-lines': [
+        'error',
+        {
+          max: 2, // Maximum number of consecutive blank lines allowed
+          maxEOF: 1, // Maximum number of blank lines allowed at the end of the file
+          maxBOF: 0, // Maximum number of blank lines allowed at the beginning of the file
+        },
+      ],
     },
   },
 ];
